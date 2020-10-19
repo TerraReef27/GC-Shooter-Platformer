fa9fbfacd9beb4d2e715c035be8ff316c19d0d34
TerraReef27 <terrareef27@gmail.com>
2020-10-19 00:59:10 -0400

    Checkpoints Added
    There are now checkpoints that will automatically set the player respawn point

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    RespawnSystem respawner = null;
+    [SerializeField] Collider2D trigger = null;
+    private bool newTrigger = true;
+
+    void Awake()
+    {
+        respawner = FindObjectOfType<RespawnSystem>();
+        if (trigger == null)
+            Debug.Log("Checkpoint trigger not set!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && newTrigger)
+        {
+            if(newTrigger)
+                ActivateCheckpoint();
+        }
+    }
+
+    private void ActivateCheckpoint()
+    {
+        newTrigger = false;
+        respawner.SetNewSpawnpoint(this.transform.position);
+    }
+}
--- Assets/Scripts/RespawnSystem.cs
+++ Assets/Scripts/RespawnSystem.cs
@@ -3,21 +3,28 @@ using UnityEngine;
 
 public class RespawnSystem : MonoBehaviour
 {
-    private Vector3 baseRespawnPoint;
+    //private Checkpoint[] resapwnPoints;
+    private Vector3 currentRespawnPoint;
     [Tooltip("The object to respawn when called")]
     [SerializeField] GameObject targetObject = null;
-    
+
     public event OnPlayerRespawnDelegate OnPlayerRespawn; //Create an event for when the player dies
     public delegate void OnPlayerRespawnDelegate(Vector3 respawnPos); //Delegate to pass on respawn information
 
     void Start()
     {
-        baseRespawnPoint = targetObject.transform.position;
+        //resapwnPoints = FindObjectsOfType<Checkpoint>();
+        currentRespawnPoint = targetObject.transform.position;
     }
 
     //Respawns the object at the base position
     public void Respawn()
     {
-        OnPlayerRespawn?.Invoke(baseRespawnPoint); //Send out event to all subscribers
+        OnPlayerRespawn?.Invoke(currentRespawnPoint); //Send out event to all subscribers
+    }
+
+    public void SetNewSpawnpoint(Vector3 newRespawnPos)
+    {
+        currentRespawnPoint = newRespawnPos;
     }
 }
