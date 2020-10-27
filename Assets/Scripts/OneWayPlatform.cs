a908a697168293caa7b32896346732f7a997ff20
BlazingMonk40 <clay.brown@bobcats.gcsu.edu>
2020-10-27 15:08:11 -0400

    Work on OneWayPlatforms

---
+++ Assets/Scripts/OneWayPlatform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayPlatform : MonoBehaviour
+{
+    public bool isAbove;
+
+    GameObject physics;
+    GameObject player;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            transform.parent.GetComponent<Collider2D>().enabled = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            transform.parent.GetComponent<Collider2D>().enabled = isAbove;
+        }
+    }
+
+}
