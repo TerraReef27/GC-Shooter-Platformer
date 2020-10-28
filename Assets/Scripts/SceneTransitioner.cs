feeb1e3a258e3a29af2c47bdb6d484a23e5094b5
TerraReef27 <terrareef27@gmail.com>
2020-10-28 17:45:28 -0400

    Adding Elevator Buttons
    Working on the elevator scene

---
+++ Assets/Scripts/SceneTransitioner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneTransitioner
+{
+    private static AssetBundle levelAssets = AssetBundle.LoadFromFile("Assets/Scenes/Levels");
+    private static string[] levels = levelAssets.GetAllScenePaths();
+
+    public static void LoadLevel(int levelNum)
+    {
+        SceneManager.LoadScene(levels[levelNum]);
+    }
+    
+    public static int GetNumLevels()
+    {
+        return levels.Length;
+    }
+}
---
+++ Assets/Scripts/UI Scripts/ElevatorButtonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorButtonManager : MonoBehaviour
+{
+    [SerializeField] private GameObject buttonTemplate = null;
+
+    private ElevatorButton[] buttons;
+
+    void Start()
+    {
+        buttons = new ElevatorButton[SceneTransitioner.GetNumLevels()];
+        for(int i=0; i<buttons.Length; i++) //Create a button for each level and assign them to the array
+        {
+            buttons[i] = Instantiate(buttonTemplate, this.transform, false).GetComponent<ElevatorButton>();
+            buttons[i].SetNumber(i);
+        }
+    }
+}
