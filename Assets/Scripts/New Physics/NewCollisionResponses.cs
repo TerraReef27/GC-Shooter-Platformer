612b4d62af08b6f3a4d47a0846d285cd3a7252ca
TerraReef27 <terrareef27@gmail.com>
2021-02-12 14:39:15 -0500

    Values for Presentation
    Updated everything for the meeting presentation

---
+++ Assets/Scripts/New Physics/NewCollisionResponses.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCollisionResponses : MonoBehaviour
+{
+    private RespawnSystem respawn = null; //Reference to the respawn system in the scene
+    private Collider2D playerCollider = null;
+
+    [Tooltip("Which collisions to ignore")]
+    [SerializeField] private List<LayerMask> ignoreList = null;
+
+    [Tooltip("Which collisions to kill object")]
+    [SerializeField] private List<LayerMask> killList = null;
+
+    void Awake()
+    {
+        if (respawn == null) respawn = FindObjectOfType<RespawnSystem>();
+        playerCollider = GetComponent<Collider2D>();
+    }
+    /*
+    //Checks if the object should ignore collisions. Returns true if the collision should be ignored
+    public bool CheckIfIgnore(string collisionTag)
+    {
+        if (ignoreList.Contains(collisionTag))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    //Checks collision to see if it should kill the object
+    public bool CheckIfKill(string collisionTag)
+    {
+        if (killList.Contains(collisionTag))
+        {
+            return false;
+        }
+
+        return true;
+    }
+    */
+}
